0e9692a6b49940d72c3040257aeacc53ec42818b
Dmitriy <Dimamakarov1941@yandex.ru>
2021-10-21 20:12:21 +0300

    update status bar and bottom bar colors

--- lib/app/data/services/app_setting_service/app_setting_service.dart
+++ lib/app/data/services/app_setting_service/app_setting_service.dart
@@ -2,12 +2,15 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_getx_hive_template/app/ui/theme/app_theme.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:logging/logging.dart';
 
 class AppSettingService extends GetxService {
   final getBox = GetStorage();
+  final log = Logger('AppSettingService');
   Future<AppSettingService> init() async {
     setStartTheme(getBox.read("isDarkTheme"));
     return this;
@@ -17,20 +20,34 @@ class AppSettingService extends GetxService {
     if (isDarkTheme == null) {
       var brightness = SchedulerBinding.instance!.window.platformBrightness;
       bool isDarkMode = brightness == Brightness.dark;
-      isDarkMode
-          ? Get.changeTheme(Themes.appThemeDataDark)
-          : Get.changeTheme(Themes.appThemeDataLight);
+      isDarkMode ? Get.changeTheme(Themes.dark) : Get.changeTheme(Themes.light);
       getBox.write("isDarkTheme", null);
+      changeStatusBarColor(isDarkMode);
     } else if (isDarkTheme == true) {
-      Get.changeTheme(Themes.appThemeDataDark);
+      Get.changeTheme(Themes.dark);
       getBox.write("isDarkTheme", true);
+      changeStatusBarColor(true);
     } else if (isDarkTheme == false) {
-      Get.changeTheme(Themes.appThemeDataLight);
+      Get.changeTheme(Themes.light);
       getBox.write("isDarkTheme", false);
+      changeStatusBarColor(false);
     }
   }
 
   bool? getThemeNow() {
     return getBox.read("isDarkTheme");
+  }
+
+  void changeStatusBarColor(bool toDark) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: toDark
+          ? Themes.dark.appBarTheme.backgroundColor
+          : Themes.light.appBarTheme.backgroundColor,
+      statusBarIconBrightness: toDark ? Brightness.light : Brightness.dark,
+      systemNavigationBarColor: toDark
+          ? Themes.dark.bottomNavigationBarTheme.backgroundColor
+          : Themes.light.bottomNavigationBarTheme.backgroundColor,
+      systemNavigationBarDividerColor: Colors.transparent,
+    ));
   }
 }
--- lib/app/ui/theme/app_theme.dart
+++ lib/app/ui/theme/app_theme.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 abstract class Themes {
-  static final ThemeData appThemeDataLight = ThemeData.light().copyWith();
+  static final ThemeData light = ThemeData.light().copyWith(
+      primaryColor: Colors.blue,
+      appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+      bottomNavigationBarTheme:
+          BottomNavigationBarThemeData(backgroundColor: Colors.grey[50]));
 
-  static final ThemeData appThemeDataDark = ThemeData.dark().copyWith(
-    colorScheme: const ColorScheme(
-      primary: Color(0xffff9800),
-      primaryVariant: Color(0xff000000),
-      secondary: Color(0xff64ffda),
-      secondaryVariant: Color(0xff00bfa5),
-      surface: Color(0xff424242),
-      background: Color(0xff616161),
-      error: Color(0xffd32f2f),
-      onPrimary: Color(0xff000000),
-      onSecondary: Color(0xff000000),
-      onSurface: Color(0xffffffff),
-      onBackground: Color(0xff000000),
-      onError: Color(0xff000000),
-      brightness: Brightness.dark,
-    ),
-  );
+  static final ThemeData dark = ThemeData.dark().copyWith(
+      colorScheme: const ColorScheme(
+        primary: Color(0xffff9800),
+        primaryVariant: Color(0xff000000),
+        secondary: Color(0xff64ffda),
+        secondaryVariant: Color(0xff00bfa5),
+        surface: Color(0xff424242),
+        background: Color(0xff616161),
+        error: Color(0xffd32f2f),
+        onPrimary: Color(0xff000000),
+        onSecondary: Color(0xff000000),
+        onSurface: Color(0xffffffff),
+        onBackground: Color(0xff000000),
+        onError: Color(0xff000000),
+        brightness: Brightness.dark,
+      ),
+      appBarTheme: AppBarTheme(backgroundColor: Colors.grey[800]),
+      bottomNavigationBarTheme:
+          BottomNavigationBarThemeData(backgroundColor: Color(0xff2e2e2e)));
 }
