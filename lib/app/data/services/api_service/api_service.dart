581e6b2e5dfdca43185fbfb0bda128f16467f0ab
Dmitriy <Dimamakarov1941@yandex.ru>
2021-10-21 20:25:50 +0300

    clean from api

--- lib/app/data/services/api_service/api_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
-
-// @Openapi(
-//     additionalProperties: AdditionalProperties(
-//         pubName: 'socialnetwork_api', pubAuthor: 'Mentoster'),
-//     inputSpecFile: 'assets/api/api.yml',
-//     generatorName: Generator.dart,
-//     outputDirectory: 'lib/app/data/seravices/api_service')
-// class FastApi extends OpenapiGeneratorConfig {}
